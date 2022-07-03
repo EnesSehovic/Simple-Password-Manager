b81f030cf15670d54348c2e59bcc4e68f44b9b53
EnesSehovic <70755657+EnesSehovic@users.noreply.github.com>
2022-07-03 15:42:15 +0200

    Update insertAllData.py

--- insertAllData.py
+++ insertAllData.py
@@ -1,4 +1,3 @@
-# File dbConn.py, wordDB.db and this one need to be in the same folder (current working directory)
 def insertData(dataDict):
     from dbConn import conn, checkIfExistsDB, createTableDB    
     #conn = sqlite3.connect(':memory:')
@@ -17,8 +16,8 @@ def insertData(dataDict):
 
 # To enter more data, add as many dictionary copies in the list as you want
 # Don't forget to change the values 
-contactListDict = [{'first': 'Chris', 'last': 'Mackleine', 'username': 'chrismckk', 'email': 'kleine91@hotmail.com', 'website': 'f95', 'password': 'vibracijass'},
-                    {'first': 'Enter your first name', 'last': 'Last name', 'username': 'User name', 'email': 'Email', 'website': 'Website', 'password': 'Password'},]
+contactListDict = [{'first': 'Enter your first name', 'last': 'Last name', 'username': 'User name', 'email': 'Email', 'website': 'Website', 'password': 'Password'},
+                  ]
 
 for i in range(len(contactListDict)):
     insertData(contactListDict[i])
